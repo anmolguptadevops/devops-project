b4e9929a25fda1cb035d35111f51665fa7fbe419
anmolguptadevops <103214795+anmolguptadevops@users.noreply.github.com>
2025-10-29 03:41:22 +0530

    Update healthCheck.groovy
    added retry in health check

--- common-libraries/vars/healthCheck.groovy
+++ common-libraries/vars/healthCheck.groovy
@@ -8,7 +8,10 @@ def call() {
             sh "kubectl rollout status deployment/${serviceName}-deployment -n  ${TARGET_NAMESPACE} --timeout=120s"
 
             echo "Curling Ingress endpoint '${INGRESS_URL}'..."
-            def httpCode = sh(script: "curl -s -o /dev/null -w \"%{http_code}\" https://${INGRESS_URL}", returnStdout: true).trim()
+            def httpCode = sh(
+                script: "curl --retry 5 --retry-delay 5 -s -o /dev/null -w \"%{http_code}\" https://${INGRESS_URL}",
+                returnStdout: true
+            ).trim()
 
             if (httpCode == "200") {
                 echo "Ingress returned HTTP 200. Deployment is healthy!"
